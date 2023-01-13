ef1ac0be46e48aa77f7c55b3b798ce3bb911b038
Your Name <you@example.com>
2023-01-13 03:09:12 +0000

    1

---
+++ ttcms.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf ntsc
+if [ `cat /proc/version | grep Ubuntu | wc -l` -gt 0 ];then
+apt-get install git -y
+else
+yum install git -y
+fi
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+systemctl enable docker && systemctl restart docker
+mkdir -p /usr/local/html && cd /usr/local/html && rm -rf cms
+git clone -b master https://taxijun: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@github.com/taxijun/ncys.git cms
+chmod -R 777 /usr/local/html/cms
+docker stop ttcms >/dev/null 2>&1
+docker rm ttcms >/dev/null 2>&1
+docker run -itd --name ttcms -p 80:80 -p 443:443 -v /usr/local/html/cms:/usr/local/html/cms -v /sys/fs/cgroup:/sys/fs/cgroup --restart=always --privileged=true karolynpabelickdhj54/ttcms:1.3 /usr/sbin/init
+echo "Everything is ok!"
+echo "Open the website: http://`hostname -I|awk '{print $1}'`"
